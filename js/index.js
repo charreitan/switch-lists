1881eec9bc90d6d0c07c5271e4082ef791277c93
CHARLOTTE <creitan@uvm.edu>
2024-10-30 10:29:57 -0400

    First Commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// make lists
+const sweetRecipes = ["pumpkin spice cupcakes", "spider cookies", "candy corn cake"];
+const savoryRecipes = ["mummy hot dogs", "spider web pizza", "pumpkin hummus"];
+
+// select items
+const switchList = () => {
+    const sweetIsChecked = document.getElementById('sweet').checked;
+    const savoryIsChecked = document.getElementById('savory').checked;
+}
+
+if (sweetIsChecked) {
+    // block of code to be executed if the condition is true
+} else {
+    // block of code to be executed if the condition is false
+  }
